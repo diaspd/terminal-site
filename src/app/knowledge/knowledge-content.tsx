1eed2d2a4cd28e798676235f41e6e019f2dcca20
Pedro Dias <pdr050203@gmail.com>
2023-12-15 17:12:39 -0300

    feat: new color to knowledge content

--- src/app/knowledge/knowledge-content.tsx
+++ src/app/knowledge/knowledge-content.tsx
@@ -10,41 +10,41 @@ export function KnowledgeContent() {
 				type="single"
 				defaultValue="item-1"	
 				collapsible
-				className="bg-gray-800 rounded-md ml-10 p-4 lg:w-1/2 w-full"
+				className="bg-zinc-800 rounded-md ml-10 p-4 lg:w-1/2 w-full"
 			>
 				<Accordion.Item className="AccordionItem" value="item-1">
-					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold text-gray-200 hover:text-gray-500 transition-all duration-500 rounded border-2 border-gray-800 focus-visible:border-gray-500 group">
+					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold text-zinc-200 hover:text-zinc-500 transition-all duration-500 rounded border-2 border-zinc-800 focus-visible:border-zinc-500 group">
             Front-end 
 						<div className="group-data-[state=open]:rotate-180 hidden group-hover:flex">
 							<ArrowFatLineDown className="w-4 h-4" />
 						</div>
 					</Accordion.Trigger>
-					<Accordion.Content className="text-gray-400 p-2 text-lg select-none">
+					<Accordion.Content className="text-zinc-400 p-2 text-lg select-none">
             HTML, CSS, JavaScript, TypeScript, React.js, 
             Next.js, React Native, Electron, UX, UI.
 					</Accordion.Content>
 				</Accordion.Item>
 
 				<Accordion.Item className="AccordionItem" value="item-2">
-					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-gray-200 hover:text-gray-500 transition-all duration-500 rounded border-2 border-gray-800 focus-visible:border-gray-500 group">
+					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-zinc-200 hover:text-zinc-500 transition-all duration-500 rounded border-2 border-zinc-800 focus-visible:border-zinc-500 group">
             Back-end
 						<div className="group-data-[state=open]:rotate-180 hidden group-hover:flex">
 							<ArrowFatLineDown className="w-4 h-4" />
 						</div>
 					</Accordion.Trigger>
-					<Accordion.Content className="text-gray-400 p-2 text-lg select-none">
+					<Accordion.Content className="text-zinc-400 p-2 text-lg select-none">
             Node.js, Nestjs, Java, Prisma, SQL, Elixer, Python.
 					</Accordion.Content>
 				</Accordion.Item>
 
 				<Accordion.Item className="AccordionItem" value="item-3">
-					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-gray-200 hover:text-gray-500 transition-all duration-500 rounded border-2 border-gray-800 focus-visible:border-gray-500 group">
+					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-zinc-200 hover:text-zinc-500 transition-all duration-500 rounded border-2 border-zinc-800 focus-visible:border-zinc-500 group">
             Other Tecnologies
 						<div className="group-data-[state=open]:rotate-180 hidden group-hover:flex">
 							<ArrowFatLineDown className="w-4 h-4" />
 						</div>
 					</Accordion.Trigger>
-					<Accordion.Content className="select-none text-gray-400 p-2 text-lg gap-3 hover:text-gray-500 transition-all duration-500">
+					<Accordion.Content className="select-none text-zinc-400 p-2 text-lg gap-3 hover:text-zinc-500 transition-all duration-500">
             Git, Github, TailwindCss, StyledComponents, Stitches, Native
             Wind, Expo, Axe (accessibility), Firebase, Figma, Design
             System, Fastify, AI
@@ -52,7 +52,7 @@ export function KnowledgeContent() {
 				</Accordion.Item>
 
 				<Accordion.Item className="AccordionItem" value="item-4">
-					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-gray-200 hover:text-gray-500 transition-all duration-500 rounded border-2 border-gray-800 focus-visible:border-gray-500 group">
+					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-zinc-200 hover:text-zinc-500 transition-all duration-500 rounded border-2 border-zinc-800 focus-visible:border-zinc-500 group">
 							
             Used on Projects Front end & Mobile
 
@@ -60,7 +60,7 @@ export function KnowledgeContent() {
 							<ArrowFatLineDown className="w-4 h-4" />
 						</div>
 					</Accordion.Trigger>
-					<Accordion.Content className="text-gray-400 p-2 text-lg select-none">
+					<Accordion.Content className="text-zinc-400 p-2 text-lg select-none">
             useState, useEffect, useContext, UseCallback, useMemo, useQuery, 
 						React hook Form, Immutability, ESLint, React Router DOM, Reducer,
             JSON Server, Axios, SSG, SSR, SEO, RadixUi, Zod, Cookies, OAuth, Redux, Zustand
@@ -68,14 +68,14 @@ export function KnowledgeContent() {
 				</Accordion.Item>
 
 				<Accordion.Item className="AccordionItem" value="item-5">
-					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-gray-200 hover:text-gray-500 transition-all duration-500 rounded border-2 border-gray-800 focus-visible:border-gray-500 group">
+					<Accordion.Trigger className="text-xl flex items-center gap-2 font-semibold mt-2 text-zinc-200 hover:text-zinc-500 transition-all duration-500 rounded border-2 border-zinc-800 focus-visible:border-zinc-500 group">
 							
             Used on Projects Back end
 						<div className="group-data-[state=open]:rotate-180 hidden group-hover:flex">
 							<ArrowFatLineDown className="w-4 h-4" />
 						</div>
 					</Accordion.Trigger>
-					<Accordion.Content className="text-gray-400 p-2 text-lg select-none">
+					<Accordion.Content className="text-zinc-400 p-2 text-lg select-none">
             DDD
 					</Accordion.Content>
 				</Accordion.Item>
