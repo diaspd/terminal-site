9cab84b7eb1fbe1b692da9c18d1130679a1f84c5
Pedro Dias <pdr050203@gmail.com>
2024-03-30 14:21:10 -0300

    rmv: images from projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
 
 import { Compiler } from "@/components/compiler"
-import Image from "next/image"
 
 export const metadata = {
 	title: "Projects",
@@ -22,14 +21,6 @@ export default function Projects() {
 							className="text-xl text-zinc-200 hover:text-zinc-600 rounded border-2 border-zinc-800 focus-visible:border-zinc-500">
                   Ignite Call
 						</Link>
-
-						<Image 
-							src="https://raw.githubusercontent.com/diaspd/ignite-call/main/public/template.svg"
-							width={800} 
-							height={600} 
-							alt="" 
-							className="w-4/6 h-full rounded hidden xl:group-hover:flex absolute right-2 top-2 object-contain"  
-						/>
 					</div>
 
 					<div className="group">
@@ -39,14 +30,6 @@ export default function Projects() {
 							className="text-xl text-zinc-200 hover:text-zinc-600 rounded border-2 border-zinc-800 focus-visible:border-zinc-500">
                   Let Me Ask
 						</Link>
-
-						<Image 
-							src="https://raw.githubusercontent.com/diaspd/let-me-ask/main/public/template.svg"
-							width={800} 
-							height={600} 
-							alt="" 
-							className="w-4/6 h-full rounded hidden xl:group-hover:flex absolute right-2 top-2 object-contain" 
-						/>
 					</div>
 
 					<div className="group">
@@ -56,14 +39,6 @@ export default function Projects() {
 							className="text-xl text-zinc-200 hover:text-zinc-600 rounded border-2 border-zinc-800 focus-visible:border-zinc-500">
               Move It
 						</Link>
-
-						<Image 
-							src="https://raw.githubusercontent.com/diaspd/move-it/main/public/template.svg"
-							width={800} 
-							height={600} 
-							alt="" 
-							className="w-4/6 h-full rounded hidden xl:group-hover:flex absolute right-2 top-2 object-contain" 
-						/>
 					</div>
 
 					<div className="group">
@@ -73,14 +48,6 @@ export default function Projects() {
 							className="text-xl text-zinc-200 hover:text-zinc-600 rounded border-2 border-zinc-800 focus-visible:border-zinc-500">
                   Design System
 						</Link>
-
-						<Image 
-							src="https://raw.githubusercontent.com/diaspd/design-system-ignite/main/public/template.png"
-							width={800} 
-							height={600} 
-							alt="" 
-							className="w-4/6 h-full rounded hidden xl:group-hover:flex absolute right-2 top-2 object-contain" 
-						/>
 					</div>
 
 					<div className="group">
@@ -90,14 +57,6 @@ export default function Projects() {
 							className="text-xl text-zinc-200 hover:text-zinc-600 rounded border-2 border-zinc-800 focus-visible:border-zinc-500">
                   Gesture Controller
 						</Link>
-
-						<Image 
-							src="https://raw.githubusercontent.com/diaspd/gesture-controller/main/template/assets/project.svg"
-							width={800} 
-							height={600} 
-							alt="" 
-							className="w-4/6 h-full rounded hidden xl:group-hover:flex absolute right-2 top-2 object-contain" 
-						/>
 					</div>
 				</div>
 			</main>
