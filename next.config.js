1b5560cebf0386892435d3510b11cb8ea6e93257
Pedro Dias <pdr050203@gmail.com>
2024-04-17 19:50:48 -0300

    rename globals.css to global.css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: 
-		{
-			domains: ["raw.githubusercontent.com"]
-		}
-	
+	images: { domains: ["raw.githubusercontent.com"]}
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css"
+import "../styles/global.css"
 
 import type { Metadata } from "next"
 import { Prompt } from "next/font/google"
